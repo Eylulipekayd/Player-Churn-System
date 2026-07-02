7dcc7e08e12ef7f7d9841bf2f2ff37cc6152cece
Eylulipekayd <eylul.ipek.007@gmail.com>
2026-07-02 22:51:03 +0300

    Kaggle veri seti eklendi ve filtrelendi

---
+++ src/kaggle_dataset.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+
+
+def find_col(columns, keywords):
+    """
+        Sütun isimleri arasında belirtilen anahtar kelimeleri içeren ilk sütunu bulur.
+        Büyük/küçük harf duyarlılığını ortadan kaldırır.
+        """
+    for col in columns:
+        if all(k in col.lower() for k in keywords):
+            return col
+    return None
+
+
+def player_data(input_path, output_path):
+
+    #Csv dosyasını okur ve sütun isimlerinde boşluk varsa siler
+    df_data = pd.read_csv(input_path)
+    df_data.columns = df_data.columns.str.strip()
+
+    # find_col fonksiyonuna keyword gider ve içinde geçen sütun bulunur
+    id_col = find_col(df_data.columns, ["id"])
+    session_col = find_col(df_data.columns, ["session"])
+    level_col = find_col(df_data.columns, ["level"])
+    purchase_col = find_col(df_data.columns, ["purchase"]) or find_col(df_data.columns, ["buy"])
+
+    # Veri setindeki isimleri, projede kullanacağım sütun isimleriyle eşleştiriyor.
+    columns_mapping = {}
+    if id_col: columns_mapping[id_col] = "OyuncuID"
+    if session_col: columns_mapping[session_col] = "OturumSayisi"
+    if level_col: columns_mapping[level_col] = "OyuncuSeviyesi"
+    if purchase_col: columns_mapping[purchase_col] = "TotalSatınAlma"
+
+
+    df_filtered = df_data.rename(columns=columns_mapping)
+
+    # Veri setinin sütunlarını gerekli sütunların içindeki isimlere göre seçip ayıklıyor.
+    gerekli_sutunlar = ["OyuncuID", "OturumSayisi", "OyuncuSeviyesi", "TotalSatınAlma"]
+    mevcut_sutunlar = [c for c in gerekli_sutunlar if c in df_filtered.columns]
+
+    # Filtrelenmiş veri setinden 10.000 satırı seçer
+    df_filtered = df_filtered[mevcut_sutunlar].sample(n=min(10000, len(df_filtered)), random_state=42).reset_index(drop=True)
+    #Filtrelenmiş veri setini kaydeder
+    df_filtered.to_csv(output_path, index=False)
+
+
+    #Veri setinin olduğu csv okuyacağı input ve filtrelenmiş halini kaydedeceğimiz output belirtir.
+if __name__ == "__main__":
+
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    input_file = os.path.join(base_dir, "data", "gaming_data.csv")
+    output_file = os.path.join(base_dir, "data", "kaggle_dataset.csv")
+
+    player_data(input_file, output_file)
