40b551900ac27c9a4b22f40b815ab17a900847a1
Eylulipekayd <eylul.ipek.007@gmail.com>
2026-07-05 20:51:21 +0300

    Kendi Veri Setim Kodu Güncellendi

--- src/my_dataset.py
+++ src/my_dataset.py
@@ -20,26 +20,34 @@ def oyuncu_profili_uret(n_players=10000):
         hedef_yas = max(12, min(hedef_yas, 60))
 
         # Hedef yaşa uygun olarak geçmişten rastgele bir doğum tarihi seçiyoruz
+        # Hedef_yas=12 olan biri için toplam_gun_yas= 12 yıl 4 ay olabilir
+        # Buna göre doğum tarihi de 06.03.2014 olur.
         toplam_gun_yas = (hedef_yas * 365) + np.random.randint(0, 365)
         dogum_tarihi = bugun - timedelta(days=toplam_gun_yas)
 
 
         # Oyuncu en erken 12. yaş gününde oyuna kaydolabilir.
+        #en_erken_kayit_tarihi= 06.03.2026 12 yaşına geldiği tarih olur.
         en_erken_kayit_tarihi = dogum_tarihi + timedelta(days=12 * 365)
 
         # Oyun 3 yıl önce çıkmış olarak ele aldım.
         oyun_acilis_tarihi = bugun - timedelta(days=1095)
 
-        # Oyuncunun kaydolabileceği tarih aralığı
+        # Oyuncunun kaydolabileceği tarih aralığıdır.
+        # baslangic_tarihi = max(06.03.2026,06.07.2023) büyük değer seçilir.
         baslangic_tarihi = max(en_erken_kayit_tarihi, oyun_acilis_tarihi)
 
         #Oyuncunun potansiyel olarak kaydolabileceği en eski gün ile bugün arasındaki toplam süredir
+        #kalan_gun_araligi = (06.07.2026-06.03.2026)'dan 120 gün gelir.
         kalan_gun_araligi = (bugun - baslangic_tarihi).days
 
         #kalan_gun_araligi de çıkan değere göre kayıt tarihi bulunur.
         if kalan_gun_araligi <= 0:
             kayit_tarihi = bugun
-        #kayıt olma tarihleri üstel olsun eşit oranlarda dağılmasın.
+
+        # Kayıt olma tarihleri üstel olsun eşit oranlarda dağılmasın.
+        # 120 için else çalışır. carpan 01*01=0,01 olsun dersek mesela gun_farki = 1,2 gündür, virgülden sonrası silinir 1 gündür.
+        # Buna göre kayit_tarihi = 07.03.2026 olur.
         else:
             carpan = np.random.random() ** 2
             gun_farki = int(carpan * kalan_gun_araligi)
@@ -48,14 +56,22 @@ def oyuncu_profili_uret(n_players=10000):
 
 
         # Artık doğum tarihi net olduğu için bugünkü yaşı küsuratlı hesaplayabiliriz
+        # yas=(06.07.2026 - 06.03.2014)/ 365.25 den 12 gelir.
         yas = int((bugun - dogum_tarihi).days / 365.25)
+        # toplam_gun = (06.07.2026 - 07.03.2026) den yaklaşık 120 bulunur.
         toplam_gun = (bugun - kayit_tarihi).days
 
-        # Veri setindeki aktif/pasif oyuncu oranını dengelemek için %50 %50 ayarlar.
+        # %50 ihtimalle aktifse toplam gün ile (yaklaşık 120) 21 arasından min olan seçilir yani 21
+        # gun_farki_bugunden = (0.21) arasından mesela 14 gelsin diyelim.
+        # Buna göre son_giris = (06.07.2026 - 14 gün) den 22.06.2026 gelir
 
         if np.random.random() < 0.5:
             gun_farki_bugunden = np.random.randint(0, min(21, toplam_gun + 1))
             son_giris = bugun - timedelta(days=gun_farki_bugunden)
+
+        #%50 ihtimalle pasif kısmında toplam gün 21 den yüksekse ki yaklaşık 120 olarak ele aldık.
+        # gun_farki_bugunden =(22,121) den 80 gelebilir mesela
+        # Buna göre son_giris =(06.07.2026 - 80 gün) den 17.04.2026 olur.
         else:
 
             if toplam_gun > 21:
@@ -67,13 +83,13 @@ def oyuncu_profili_uret(n_players=10000):
 
         # girilmeyen gün sayısına göre churn hesabı yapılır 21 ve sonrası churn durumundadır.
         girilmeyen_gun_sayisi = (bugun - son_giris).days
-        is_churn = 1 if girilmeyen_gun_sayisi > 21 else 0
+        churn = 1 if girilmeyen_gun_sayisi > 21 else 0
 
         # öğrenci yaş grubunu bulur.
-        is_ogrenci = 12 <= yas <= 22
+        ogrenci_mi = 12 <= yas <= 22
         mevsimsel_saatler = {"Ilkbahar": 0, "Yaz": 0, "Sonbahar": 0, "Kış": 0}
 
-        #yaz mevsiminde öğrenci oyun saati çarpanı artar.
+        # yaz mevsiminde öğrenci oyun saati çarpanı artar.
         # Günlük oyun saati mevsimlere göre hesaplanır ve sözlüğe eklenir.
         current = kayit_tarihi
         while current <= son_giris:
@@ -81,7 +97,7 @@ def oyuncu_profili_uret(n_players=10000):
             mevsim = "Ilkbahar" if ay in [3, 4, 5] else (
                 "Yaz" if ay in [6, 7, 8] else ("Sonbahar" if ay in [9, 10, 11] else "Kış"))
 
-            carpan = 3.5 if (mevsim == "Yaz" and is_ogrenci) else (1.2 if mevsim == "Yaz" else 1.0)
+            carpan = 3.5 if (mevsim == "Yaz" and ogrenci_mi) else (1.2 if mevsim == "Yaz" else 1.0)
             gunluk_saat = np.random.normal(0.5 * carpan, 0.1)
             mevsimsel_saatler[mevsim] += max(0, gunluk_saat)
             current += timedelta(days=1)
@@ -90,7 +106,7 @@ def oyuncu_profili_uret(n_players=10000):
         #oyuncu seviyesi toplam oyun saatine bağlı olarak mantıklı şekilde hesaplanır.
         toplam_saat = sum(mevsimsel_saatler.values())
         seviye = max(1, min(100, int(np.sqrt(toplam_saat) * 2)))
-        hesap_yasi = max(0.1, round(toplam_gun / 365.25, 2))
+        hesap_yasi = max(0.1, round(toplam_gun / 365.25, 1))
 
         #tüm bulunan veriler oyuncu verisi listesine eklenir ve dataframe yani tablo oluşturulur.
         oyuncu_verisi.append({
@@ -105,7 +121,7 @@ def oyuncu_profili_uret(n_players=10000):
             "Kis_Saati": int(mevsimsel_saatler["Kış"]),
             "Toplam_Saat": int(toplam_saat),
             "OyuncuSeviyesi": seviye,
-            "Churn_Durumu": is_churn,
+            "Churn_Durumu": churn,
             "Hesap_Yasi": hesap_yasi
         })
 
